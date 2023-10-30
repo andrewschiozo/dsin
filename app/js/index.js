5c8139c350925e774b03bda18fa0b5219e945074
Andrews <andrewschiozo@gmail.com>
2023-10-30 17:37:23 -0300

    feat: método $.request (personalização do $.ajax do jQuery para chamada da camada de serviço)

--- app/js/index.js
+++ app/js/index.js
@@ -40,3 +40,48 @@ $(function(){
 
     loadView('views/login/login')
 })
+
+function loadView(view){
+    $('#view').html('')
+    $.get(view + '.html', function(data){
+        $('#view').html(data)
+    })
+}
+
+function sair(){
+    Leila.sair()
+    loadView('views/login/login')
+}
+
+$.requestService = function({route, method = 'GET', data = {}, resource = null, async = true} = {}, callback){
+
+    let config = {
+        url: GLOBALS.baseUrlService + route
+        ,type: method
+        ,contentType: 'application/json'
+        ,data: JSON.stringify({data: data, resource: resource})
+        ,async: async
+        ,dataType: 'json'
+        ,success: function(response){
+            callback(response)
+        }
+        ,error: function(request){
+            $.notify({message: 'Wooops! Não foi possível fazer isso'},{type: 'danger'})
+        }
+        ,complete: function(request)
+        {
+            let notificacaoMensagem = ''
+            if(request.responseJSON.message.length > 0)
+            {
+                $.each(request.responseJSON.message, function(index, mensagem) {
+                    notificacaoMensagem += mensagem + '; '
+                })
+                $.notify({message: notificacaoMensagem},{type: request.responseJSON.status ? 'success' : 'danger'})
+            }
+        }
+
+    }
+    config.headers = route !== 'Login' ? {'Authorization': 'Bearer ' + Leila.get().token} : null
+
+    $.ajax(config)
+}
