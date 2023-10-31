1e192ccf904ac907810b78d0504df114a8e34941
Andrews <andrewschiozo@gmail.com>
2023-10-31 16:49:56 -0300

    feat: tratamento do menu

---
+++ app/views/home/home.js
@@ -0,0 +1,29 @@
+setMenu()
+
+//Funções
+function setMenu()
+{
+    $.each(Leila.get().usuario.data.menu, function(index, menu){
+        $('#menus').append($('<div>').addClass('col-1')
+                .append($('<button>').addClass('btn-menu btn')
+                                     .addClass(menu.nome == 'Home' ? 'btn-dark' : '')
+                                     .attr('view', menu.href)
+                                     .append($('<i>').addClass(menu.icone))
+                                     .append(menu.nome)))
+    })
+}
+
+//Eventos
+$('.btn-menu').click(function(){
+    let view = $(this).attr('view')
+
+    $('.btn-menu').removeClass('btn-dark')
+    $(this).addClass('btn-dark')
+    
+    if(view == 'home/home')
+    {
+        loadView(view, '#view')
+        return
+    }
+    loadView(view)  
+})
