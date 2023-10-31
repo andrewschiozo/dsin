9652057f1e9ffe269de1f09dc25104ae1c769161
Andrews <andrewschiozo@gmail.com>
2023-10-31 11:48:30 -0300

    feat: request login

---
+++ app/views/login/login.js
@@ -0,0 +1,25 @@
+requestConfig = {
+    route: 'Login'
+}
+
+$('#btn-login').click(function(){
+    requestConfig.method = 'POST'
+    requestConfig.data = {
+         usuario: $('#login-usuario').val()
+        ,senha: $('#login-senha').val()
+    }
+    
+    $.requestService(requestConfig, function(response){
+        if(!response.status)
+            return
+
+        $.notify({message: 'Sucesso!'},{type: 'success'})
+
+        Leila.data.logado = true
+        Leila.data.token = response.data.token
+        Leila.data.usuario = parseJwt(response.data.token)
+        Leila.save()
+
+        loadView('home/home')
+    })
+})
