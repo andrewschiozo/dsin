ca9ef92edebf7d6de7cbbf9e4d60c47679bacef5
Andrews <andrewschiozo@gmail.com>
2023-11-01 10:22:07 -0300

    feat: insert/update

--- service/dao/DaoUsuario.php
+++ service/dao/DaoUsuario.php
@@ -20,6 +20,7 @@ class DaoUsuario extends DB
         $this->table = 'usuario';
         $this->model = new ModelUsuario;
         $this->dao = __CLASS__;
+        $this->blackList[] = 'senha';
         parent::__construct();
     }
 
@@ -45,5 +46,71 @@ class DaoUsuario extends DB
             echo ($e->getMessage());
         }
         return $result;
+    }
+
+    public function save($id = null)
+    {
+        return $id ? $this->update($id) : $this->insert();
+    }
+
+    private function insert()
+    {
+        
+        $queryUsuario = 'INSERT INTO ' . $this->table . ' (nome, email, telefone, perfil, usuario, senha)
+        VALUES (:nome, :email, :telefone, :perfil, :usuario, :senha)';
+        $usuarioId = false;
+        
+        try {
+            $this->getConn()->beginTransaction();
+
+            $stmt = $this->getConn()->prepare($queryUsuario);
+            $stmt->bindValue(':nome', $this->model->nome, \PDO::PARAM_STR);
+            $stmt->bindValue(':email', $this->model->email, \PDO::PARAM_STR);
+            $stmt->bindValue(':telefone', $this->model->telefone, \PDO::PARAM_INT);
+            $stmt->bindValue(':perfil', $this->model->perfil, \PDO::PARAM_STR);
+            $stmt->bindValue(':usuario', $this->model->usuario, \PDO::PARAM_STR);
+            $stmt->bindValue(':senha', $this->model->senha, \PDO::PARAM_STR);
+
+            $stmt->execute();
+            $usuarioId = $this->getConn()->lastInsertId();
+                        
+            $this->getConn()->commit();            
+        } catch (\PDOException $e) {
+            $this->getConn()->rollback();
+            echo ($e->getMessage());
+        }
+        return $usuarioId;
+    }
+
+    private function update($id)
+    {
+        $queryUsuario = 'UPDATE ' . $this->table . ' SET nome = :nome
+                                                       ,email = :email
+                                                       ,telefone = :telefone
+                                                       ,perfil = :perfil
+                                                       ,usuario = :usuario
+                                                       ,senha = :senha
+                                                    WHERE id = :id';
+        
+        try {
+            $this->getConn()->beginTransaction();
+
+            $stmt = $this->getConn()->prepare($queryUsuario);
+            $stmt->bindValue(':nome', $this->model->nome, \PDO::PARAM_STR);
+            $stmt->bindValue(':email', $this->model->email, \PDO::PARAM_STR);
+            $stmt->bindValue(':telefone', $this->model->telefone, \PDO::PARAM_INT);
+            $stmt->bindValue(':perfil', $this->model->perfil, \PDO::PARAM_STR);
+            $stmt->bindValue(':usuario', $this->model->usuario, \PDO::PARAM_STR);
+            $stmt->bindValue(':senha', $this->model->senha, \PDO::PARAM_STR);
+            $stmt->bindValue(':id', $id, \PDO::PARAM_INT);
+
+            $stmt->execute();
+            
+            $this->getConn()->commit();            
+        } catch (\PDOException $e) {
+            $this->getConn()->rollback();
+            echo ($e->getMessage());
+        }
+        return $id;
     }
 }
