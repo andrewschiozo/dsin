a9603800c244a96b58125c792dbf3a703de00ed9
Andrews <andrewschiozo@gmail.com>
2023-11-02 18:37:15 -0300

    feat: inclusao de campo para gerenciar situacao do agendamento

--- ddl.sql
+++ ddl.sql
@@ -4,7 +4,7 @@ CREATE TABLE usuario(
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    telefone INT NOT NULL,
+    telefone BIGINT(20) NOT NULL,
     perfil VARCHAR(20) NOT NULL,
     usuario varchar(20) NOT NULL,
     senha VARCHAR(255) NOT NULL
@@ -27,6 +27,7 @@ CREATE TABLE agendamento(
     servico_id INT NOT NULL,
     usuario_id INT NOT NULL,
     data DATETIME NOT NULL
+    servico_situacao_id INT DEFAULT 1,
 );
 ALTER TABLE agendamento ADD CONSTRAINT fk_servico_id FOREIGN KEY (servico_id) REFERENCES servico(id);
 ALTER TABLE agendamento ADD CONSTRAINT fk_usuario_id FOREIGN KEY (usuario_id) REFERENCES usuario(id);
